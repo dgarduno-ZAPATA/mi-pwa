0e5dc22c177f4070c5b810c57c786d53c76e5d86
dgarduno-ZAPATA <dgarduno@zapata.com.mx>
2025-08-19 12:01:46 -0600

    Create pdfGenerator.js

---
+++ tils/pdfGenerator.js
@@ -0,0 +1,7 @@
+// utils/pdfGenerator.js
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+import { formatCurrency, formatNumber, formatPercentage } from './calculations'
+
+// Generar PDF con cálculo TCO
+export async function generateT
